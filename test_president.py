cde4535e09f1f4ef615f1b4d6b0143da5af71ffe
B0G0311 <ptgarland@protonmail.com>
2022-11-06 19:32:48 -0500

    Completed The test variables

--- test_president.py
+++ test_president.py
@@ -1,19 +1,32 @@
-import unittest
+import pytest
 import requests
 
 url_ddg = "https://api.duckduckgo.com"
 
 
-def test_ddg0():
-    resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
-    rsp_data = resp.json()
-    assert "DuckDuckGo" in rsp_data["Heading"]
-
-
-def test_ddg1():
+@pytest.fixture
+def pres_resp():
     resp = requests.get(url_ddg + "/?q=presidents+of+the+united+states&format=json")
     rsp_data = resp.json()
-    assert "Presidents of the United States" in rsp_data["Heading"]
+    return rsp_data
 
 
+@pytest.fixture
+def pres_list():
+    presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Jackson", "Buren", "Harrison", "Tyler",
+                  "Polk", "Taylor", "Fillmore", "Pierce", "Buchanan", "Lincoln", "Johnson", "Grant", "Hayes",
+                  "Garfield", "Arthur", "Cleveland", "Harrison", "McKinley", "Roosevelt", "Taft", "Wilson", "Harding",
+                  "Coolidge", "Hoover", "Truman", "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter",
+                  "Reagan", "Bush", "Clinton", "Obama", "Trump", "Biden"]
 
+    return presidents
+
+
+def test_ddg2(pres_resp, pres_list):
+    topics = pres_resp["RelatedTopics"]
+    resulttext = ""
+    for topic in topics:
+        resulttext += topic["Text"]
+
+    for president in pres_list:
+        assert president in resulttext
